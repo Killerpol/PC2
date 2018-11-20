3e854b51b1c31718cf931a3135bc3353dd0d1e69
Jose Luis <aldairxd11@hotmail.com>
2018-11-20 08:38:47 -0500

    p3

---
+++ p3.py
@@ -0,0 +1,9 @@
+matriz = [[1,2,0] , [3,1,4] , [3,0,1]]
+
+
+print (matriz [1] [1] )
+print (matriz[2][2])
+print (matriz[3][3])
+
+suma = matriz [1] [1] + matriz [2] [2] + matriz [3] [3]
+print ( suma )
