51c93de5ff748282b727fdda5ad9bae4374e605f
Jose Luis <aldairxd11@hotmail.com>
2018-11-20 08:25:34 -0500

    p2

---
+++ p2.py
@@ -0,0 +1,24 @@
+#Arreglo
+lista = []
+mayor = 0
+
+
+def menor_numero_arreglo () :
+    
+    for i in range (5):
+        numero = int(input("ingrese un numero"))
+        lista.append(numero) 
+    if numero > mayor:
+        numero = mayor
+        
+
+    print (mayor)
+    print (lista)
+    
+
+menor_numero_arreglo()
+
+
+
+
+
